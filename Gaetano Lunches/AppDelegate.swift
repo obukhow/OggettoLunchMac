90c5ac1a43bd9557053a19a0bfc1feb5c9933ff0
Denis Obukhov <denis.obukhov@oggettoweb.com>
2019-07-07 14:45:09 +0300

    Initial Commit

---
+++ Gaetano Lunches/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Gaetano Lunches
+//
+//  Created by Денис Обухов on 07/07/2019.
+//  Copyright © 2019 Oggetto. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Gaetano Lunches/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Gaetano Lunches
+//
+//  Created by Денис Обухов on 07/07/2019.
+//  Copyright © 2019 Oggetto. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
